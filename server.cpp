80febe51dd69d8544e0579ea1c3ab446d55ff40d
alknn <ozdemirumitalkan@gmail.com>
2023-11-08 16:11:52 +0300

    Server Created

--- server.cpp
+++ server.cpp
@@ -1,7 +1,29 @@
+
+#include <array>
+#include <cstdlib>
+#include <iostream>
+#include <memory>
+#include <type_traits>
+#include <utility>
+
 #include <boost_1_81_0/boost/filesystem.hpp>
 #include <boost_1_81_0/boost/asio.hpp>
 
 using boost::asio::ip::tcp;
+
+class handler_memory 
+{
+public:
+    handler_memory() : in_use_false(false) {}
+    handler_memory(const handler_memory&) = delete;
+    handler_memory& operator=(const handler_memory&) = delete;
+
+    void * allocate(std::size_t size)
+    {
+        
+    }
+
+}
 
 
 
